9eaf072e0445861937fc1386edfbebb534af3fe6
Ahmet Kadir Çiçek <104458700+ahmetkadircicek@users.noreply.github.com>
2022-05-01 18:38:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function stratocaster() {
+  location.replace("http://127.0.0.1:5500/WEB/Whammy/stratocaster.html")
+}
+
+function telecaster() {
+  location.replace("http://127.0.0.1:5500/WEB/Whammy/telecaster.html")
+}
+
+function accessories() {
+  location.replace("http://127.0.0.1:5500/WEB/Whammy/accessories.html")
+}
+
+function parts() {
+  location.replace("http://127.0.0.1:5500/WEB/Whammy/parts.html")
+}
+
+function contactme() {
+  location.replace("http://127.0.0.1:5500/WEB/Whammy/contactme.html")
+}
+
+function whammy() {
+  location.replace("http://127.0.0.1:5500/WEB/Whammy/whammy.html")
+}
+
+function notfound() {
+  location.replace("http://127.0.0.1:5500/WEB/Whammy/notfound.html")
+}
